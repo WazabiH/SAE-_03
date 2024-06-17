e0cf87f069d5158e0fc35d974ee602f93fdd2b3d
WazabiH <hamedwassila2004@gmail.com>
2024-06-17 17:29:45 +0200

    Update auteur.php

--- code/auteur.php
+++ code/auteur.php
@@ -26,21 +26,20 @@ $articles = mysqli_fetch_all($resultat_articles, MYSQLI_ASSOC);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Auteur de <?php echo htmlspecialchars_decode($auteur['nom'] . ' ' . $auteur['prenom']); ?> - SAÉ 203</title>
+    <title>Articles de <?php echo htmlspecialchars_decode($auteur['nom'] . ' ' . $auteur['prenom']); ?> - SAÉ 203</title>
 
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/reset.css">
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/fonts.css">
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/global.css">
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/header.css">
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/accueil.css">
-    <link  rel= "icône de raccourci"  href= "/favicon.ico"  type= "image/x-icon" > 
-    <link  rel= "icon"  href= "favicon.ico"  type= "image/x-icon" >
+    <link rel="icon" href="ressources/images/favicon.ico" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 
 <body>
     <?php require_once('./ressources/includes/top-navigation.php'); ?>
-    <?php require_once('./ressources/includes/bulle.php');?>
+    <?php require_once('./ressources/includes/bulle.php'); ?>
 
     <main class="container mx-auto px-4 py-8">
         <div class="flex items-center justify-center mb-8">
@@ -66,7 +65,9 @@ $articles = mysqli_fetch_all($resultat_articles, MYSQLI_ASSOC);
     </div>
             <?php endforeach; ?>
         </div>
-    </main> 
     </main>
-</body> 
+
     <?php require_once('./ressources/includes/footer.php'); ?>
+</body>
+
+</html>
