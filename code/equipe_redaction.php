48fe627304916569aacd73215e19a4b141031bda
WazabiH <hamedwassila2004@gmail.com>
2024-06-17 17:16:48 +0200

    Update equipe_redaction.php

--- code/equipe_redaction.php
+++ code/equipe_redaction.php
@@ -1,14 +1,14 @@
 <?php
-$couleur_bulle_classe = "violet";
+$couleur_bulle_classe = "rose";
 $page_active = "equipe";
 
 require_once('./ressources/includes/connexion-bdd.php');
 
+// Vos requêtes SQL
 $requete_auteurs = "SELECT id, nom, prenom, lien_avatar FROM auteur";
 $resultat_auteurs = mysqli_query($mysqli_link, $requete_auteurs);
 
 $auteurs = mysqli_fetch_all($resultat_auteurs, MYSQLI_ASSOC);
-
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -18,24 +18,26 @@ $auteurs = mysqli_fetch_all($resultat_auteurs, MYSQLI_ASSOC);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Équipe de rédaction - SAÉ 203</title>
+    <title>Equipe de Rédaction - SAÉ 203</title>
 
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/reset.css">
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/fonts.css">
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/global.css">
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/header.css">
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/accueil.css">
+    <link rel="stylesheet" href="ressources/css/equipederedaction.css">
 
-    <link rel="stylesheet" href="ressources/css/equipe.css">
     <link rel="stylesheet" href="ressources/css/global.css">
+    <link rel="stylesheet" href="ressources/css/equipe.css">
+    
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 
-    <link  rel= "icône de raccourci"  href= "/favicon.ico"  type= "image/x-icon" > 
-    <link  rel= "icon"  href= "favicon.ico"  type= "image/x-icon" >
+
 </head>
 
 <body>
     <?php require_once('./ressources/includes/top-navigation.php'); ?>
-    <?php require_once('./ressources/includes/bulle.php');?>
+    <?php require_once('./ressources/includes/bulle.php'); ?>
 
     <main class="conteneur-principal conteneur-1280">
         <div class="container">
@@ -57,6 +59,7 @@ $auteurs = mysqli_fetch_all($resultat_auteurs, MYSQLI_ASSOC);
             </div>
         </div>
     </main>
+
     <?php require_once('./ressources/includes/footer.php'); ?>
 </body>
 
